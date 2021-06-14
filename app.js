631d27ebc8780d6e522e3bfb64885332668b060b
Joe Santos <jaysantos012@gmail.com>
2021-06-14 00:50:42 -0400

    was able to finish the project.

--- app.js
+++ app.js
@@ -12,7 +12,45 @@ recognition.start();
 
 function onSpeak(e) {
     const msg = e.results[0][0].transcript;
-    console.log(msg)
+    writeMessage(msg);
+    checkNumber(msg);
+}
+
+function writeMessage(msg) {
+    msgEl.innerHTML = `
+    <div>You said: </div>
+    <span class='box'>${msg}</span>
+    `;
+}
+function checkNumber(msg) {
+    const num = +msg;
+
+    // Check if the vaild number
+    if (Number.isNaN(num)) {
+        msgEl.innerHTML += '<div>That is not a valid number</div>';
+        return
+    }
+
+    //check in range
+
+    if (num > 100 || num < 1) {
+        msgEl.innerHTML += '<div>Number must be between 1 and 100</div>';
+        return;
+    }
+
+    // check number
+
+    if (num === randomNum) {
+        document.body.innerHTML = `
+        <h2>Congrats! You have gussed the number! <br><br>
+        It was ${num}</h2>
+        <button class="play-again" id="play-again">Play Again</button>
+        `;
+    } else if (num > randomNum) {
+        msgEl.innerHTML += '<div>Go LOWER</div>';
+    } else {
+        msgEl.innerHTML += '<div>Go HIGHER</div>';
+    }
 }
 
 function getRandomNumber() {
@@ -20,4 +58,12 @@ function getRandomNumber() {
 }
 
 recognition.addEventListener('result', onSpeak);
+
+recognition.addEventListener('end', () => recognition.start());
+
+document.body.addEventListener('click', e => {
+    if(e.target.id == 'play-again') {
+        window.location.reload();
+    }
+});
 

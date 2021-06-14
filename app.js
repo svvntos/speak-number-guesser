a55f35edcb2fb617cddc17e82ea1cd6f6d34b985
Joe Santos <jaysantos012@gmail.com>
2021-06-14 00:35:38 -0400

    was able to get the mic input to display on console log, now to compare the numbers with input

---
+++ app.js
@@ -0,0 +1,23 @@
+const msgEl = document.getElementById('msg');
+
+const randomNum = getRandomNumber();
+
+console.log('Number:', getRandomNumber());
+
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+let recognition = new window.SpeechRecognition();
+
+recognition.start();
+
+function onSpeak(e) {
+    const msg = e.results[0][0].transcript;
+    console.log(msg)
+}
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * 100) + 1;
+}
+
+recognition.addEventListener('result', onSpeak);
+
